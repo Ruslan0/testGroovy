6441a853c7d145e57204f0db3e624d774a110e7c
paluektau <rvvd@mail.ru>
2018-04-06 10:09:27 +0300

    Refactoring

--- src/TestTask1.groovy
+++ src/TestTask1.groovy
@@ -2,60 +2,60 @@ import java.util.regex.Pattern
 
 class TestTask1 {
 
-    static  Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile( /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, Pattern.DOTALL)
-    static  def str="I want to walk my DOG, Cat, and asrtatrsa and qwertrewq Maybe even my tortoise@mail.ru ."
+    static Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile(/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, Pattern.DOTALL)
+    static def str = "I want to walk my DOG, Cat, and asrtatrsa and qwertrewq Maybe even my tortoise@mail.ru ."
 
     static def getWordsOnlyLwerCase() {
-        return str.split().findAll{ word->word.equals(word.toLowerCase()) }
+        return str.split().findAll { word -> word == word.toLowerCase() }
     }
 
     static def getStrFrom11To16(String str) {
-        if (str.length()>=16)
-            return str [11..16]
+        if (str.length() >= 16)
+            return str[11..16]
         else
             return ""
     }
 
     static def getEnglishLetters(String s) {
-		return s.toCharArray().findAll{ ch -> ((ch >= 'a') && (ch <= 'z')) }
-	}
+        return s.toCharArray().findAll { ch -> ((ch >= 'a') && (ch <= 'z')) }
+    }
 
     static def getreplaceFirstSymboltoUp(String s) {
-        return s.split().collect{ it.capitalize()}.join(" " )
+        return s.split().collect { it.capitalize() }.join(" ")
     }
 
     static def getReverse(String target) {
-        return target.tokenize().findAll{ it.toSet().size()<=10 && it == it.reverse() }
+        return target.tokenize().findAll { it.toSet().size() <= 5 && it == it.reverse() }
     }
 
 
     static def isContainEmail(String s) {
-		return s.split().any{ VALID_EMAIL_ADDRESS_REGEX.matcher(it).find() }
-	}
+        return s.split().any { VALID_EMAIL_ADDRESS_REGEX.matcher(it).find() }
+    }
 
     static def task11(String str) {
         println wordsOnlyLwerCase      // а
-        println getStrFrom11To16(str)		// б
-	    println getEnglishLetters(str)		// в
-	    println getreplaceFirstSymboltoUp(str)  // г
+        println getStrFrom11To16(str)        // б
+        println getEnglishLetters(str)        // в
+        println getreplaceFirstSymboltoUp(str)  // г
         println getReverse(str)             // д
-        println(isContainEmail(str))		// е
-	}
+        println(isContainEmail(str))        // е
+    }
 
     static def task12(Integer[] intArray) {
-        Integer[] intArray2 = [220, 300, 110,-122, 110,2,3,332,2,6, -1]
-        println "unique size = "+intArray.toUnique().size()									// а
-        println "max = "+intArray.toUnique().max() + "; min = "+intArray.toUnique().min()		// б
-        println intArray.collect ( { return it>0?it*2:it*3 })										// в
-        println intArray.collect().intersect(intArray2.collect())								// г
-	}	
-	
-	static void main(args) {
-        Integer[] intArray = [200, 300, 100,-122, 100,2,1,331,2,5, -1]
+        Integer[] intArray2 = [220, 300, 110, -122, 110, 2, 3, 332, 2, 6, -1]
+        println "unique size = " + intArray.toUnique().size()                                    // а
+        println "max = " + intArray.toUnique().max() + "; min = " + intArray.toUnique().min()        // б
+        println intArray.collect({ return it > 0 ? it * 2 : it * 3 })                                        // в
+        println intArray.collect().intersect(intArray2.collect())                                // г
+    }
+
+    static void main(args) {
+        Integer[] intArray = [200, 300, 100, -122, 100, 2, 1, 331, 2, 5, -1]
         println "*** Task 1.1 ***"
         task11(str)
         println "*** Task 1.2 ***"
         task12(intArray)
-	}
-  
+    }
+
 }
--- src/TestTask3.groovy
+++ src/TestTask3.groovy
@@ -1,48 +1,47 @@
-import java.nio.file.Path
-import groovy.xml.MarkupBuilder
-
 class TestTask3 {
 
     static def getBaseFileName(String fileName) {
-		if (fileName.indexOf(".") > 0) {
-			return fileName.substring(0, fileName.lastIndexOf("."))
-		 } else {
-			return fileName
-		 }
-	}
-	
-	static def getDir(String fileName) {
-		if (fileName.indexOf("\\") > 0) {
-			return fileName.substring(0, fileName.lastIndexOf("\\"))
-		 } else {
-			return fileName
-		 }
-	}
+        if (fileName.indexOf(".") > 0) {
+            return fileName.substring(0, fileName.lastIndexOf("."))
+        } else {
+            return fileName
+        }
+    }
+
+    static def getDir(String fileName) {
+        if (fileName.indexOf("\\") > 0) {
+            return fileName.substring(0, fileName.lastIndexOf("\\"))
+        } else {
+            return fileName
+        }
+    }
 
     static def findFilesGroovy(dir) {
-		def mask = /^0|[1-9]\d*\.txt$/
-		
-		def groovyFiles = new FileNameByRegexFinder().getFileNames(dir, mask)
+        def mask = /^0|[1-9]\d*\.txt$/
+
+        def groovyFiles = new FileNameByRegexFinder().getFileNames(dir, mask)
         println groovyFiles
         groovyFiles.each {
             File f = new File(it);
             println f.parentFile.toURI().relativize(f.toURI()).getPath()
             def baseFileName = getBaseFileName(f.getName()).reverse()
-            def newFileName = baseFileName.reverse()+'_'+baseFileName.collect { return it.toInteger() }.sum()+".dat"
-            if (f.renameTo(getDir(f.getPath())+"/"+newFileName))
-                println f.getName() + " has been renamed to " +  newFileName
+            def newFileName = baseFileName.reverse() + '_' + baseFileName.collect {
+                return it.toInteger()
+            }.sum() + ".dat"
+            if (f.renameTo(getDir(f.getPath()) + "/" + newFileName))
+                println f.getName() + " has been renamed to " + newFileName
             else
-                println f.getName() + " hasn't been renamed to " +  +newFileName
-		}
+                println f.getName() + " hasn't been renamed to " + +newFileName
+        }
 
-	}
+    }
 
-	static main(args) {
-		if (args.size()==0) {
-			println ("the directory is not found")
-			return
-		}
-		findFilesGroovy(args[0])
-	}
+    static main(args) {
+        if (args.size() == 0) {
+            println("the directory is not found")
+            return
+        }
+        findFilesGroovy(args[0])
+    }
 
 }
